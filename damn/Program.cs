b29fe3f37700153ed7bbdfcc828fc4a98e4c2928
Сергей Усов <sergej.usov.04@mail.ru>
2020-03-18 10:10:15 +0300

    минимальное из трех чисел

---
+++ damn/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace damn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region
+            /*
+            int a, b, c;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = Convert.ToInt32(Console.ReadLine());
+            if (a > b) a = b;
+            if (a > c) a = c;
+            Console.WriteLine("Min = {0}", a);
+            Console.ReadKey();
+            */
+            #endregion
+
+
+
+
+
+        }
+    }
+}
